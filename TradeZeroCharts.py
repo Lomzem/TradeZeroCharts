2a545b04dd906cdd813675966ac4f0acd6340186
Lawjay Lee <lawjaylee1@icloud.com>
2023-08-02 14:56:38 -0700

    add fromcsv

--- TradeZeroCharts.py
+++ TradeZeroCharts.py
@@ -1,1 +1,13 @@
+import pandas as pd
+import numpy as np
+
+from lomzem_env import *
+
 class TradeZeroCharts:
+    def __init__(self):
+        self.df = None
+    def from_csv(self, csv_path: str) -> None:
+        self.df = pd.DataFrame(csv_path)
+
+test = TradeZeroCharts()
+test.from_csv('./TradeHistory20230802214519.csv')
