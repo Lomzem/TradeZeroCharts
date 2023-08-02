8966e01dc3e84fd6cfc331285a18773bb4340d51
Lawjay Lee <lawjaylee1@icloud.com>
2023-08-02 15:09:29 -0700

    more from_csv

--- TradeZeroCharts.py
+++ TradeZeroCharts.py
@@ -1,3 +1,5 @@
+import datetime
+
 import pandas as pd
 import numpy as np
 
@@ -7,7 +9,9 @@ class TradeZeroCharts:
     def __init__(self):
         self.df = None
     def from_csv(self, csv_path: str) -> None:
-        self.df = pd.DataFrame(csv_path)
+        self.df = pd.read_csv(csv_path, usecols=['T/D', 'Side', 'Symbol', 'Qty', 'Price', 'Exec Time'], parse_dates=['T/D', 'Exec Time'], dtype={'Side': 'category', 'Qty': np.uint16, 'Price': np.float32})
 
 test = TradeZeroCharts()
-test.from_csv('./TradeHistory20230802214519.csv')
+test.from_csv(trade_history_file)
+print((test.df))
+print(test.df.info())
